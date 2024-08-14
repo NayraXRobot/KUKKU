20edecc7b43168233baeb3417ec8e50ea8cad41c
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-08-14 14:22:42 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -55,7 +55,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/TheTeamVivek/Extra-Plugin",
+    "https://github.com/NayraXRobot/Nayra11",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
